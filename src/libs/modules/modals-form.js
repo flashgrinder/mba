5384c4afc3d06c618e9f83a2cb094527f96d2fc7
flashgrinder <flashgrinder92@gmail.com>
2022-01-21 17:06:45 +0400

    make html modals form

--- src/libs/modules/modals-form.js
+++ src/libs/modules/modals-form.js
@@ -2,11 +2,29 @@ import HystModal from 'hystmodal';
 
 function init(container = document) {
 
+    const modalBtnNext = document.querySelector('.modal__btn--next');
+    const modalWrapperFirst = document.querySelector('.js-wrapper-first');
+    const modalWrapperLast = document.querySelector('.js-wrapper-last');
+
+    function modalWrapperShiftNext () {
+        modalWrapperFirst.classList.add('is-close');
+        modalWrapperLast.classList.add('is-open');
+    }
+
+    function modalWrapperShiftBack () {
+        modalWrapperFirst.classList.remove('is-close');
+        modalWrapperLast.classList.remove('is-open');
+    }
+    modalBtnNext.addEventListener('click', modalWrapperShiftNext);
+
     const modalsForms = new HystModal({
         linkAttributeName: "data-hystmodal",
         catchFocus: true,
         waitTransitions: true,
         backscroll: false,
+        afterClose: function(modal){
+            modalWrapperShiftBack();
+        },
     });
         
 }
